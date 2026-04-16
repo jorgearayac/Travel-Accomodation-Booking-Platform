be67f4ff1f670dccc0e47b57c2d4a67b5b2c1603
jorgearayac <araya.cepeda.jorge@gmail.com>
2026-04-16 16:17:34 -0400

    add: connection string to web API project & first migration. HotelBookingDb database created

--- HotelBooking.API/Program.cs
+++ HotelBooking.API/Program.cs
@@ -1,4 +1,10 @@
+using HotelBooking.Db.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelBookingDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
