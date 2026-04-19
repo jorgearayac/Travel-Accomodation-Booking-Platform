a000be3c295cb3e2fe6c0259fc6cd953173f67bc
jorgearayac <araya.cepeda.jorge@gmail.com>
2026-04-19 16:30:45 -0400

    add: auth DTOs

---
+++ HotelBooking.API/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBooking.API.DTOs.Auth;
+
+public class AuthResponse
+{
+    public required string Token { get; set; }
+    public int UserId { get; set; }
+    public required string Username { get; set; }
+    public required string Role { get; set; }
+}
---
+++ HotelBooking.API/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBooking.API.DTOs.Auth;
+
+public class RegisterRequest
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
