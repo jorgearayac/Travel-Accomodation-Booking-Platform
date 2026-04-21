6fb06a0d6664fd95d56a820d58e793ea2db58758
jorgearayac <araya.cepeda.jorge@gmail.com>
2026-04-21 12:13:03 -0400

    add: City repository

---
+++ HotelBooking.Db/Interfaces/ICityRepository.cs
@@ -0,0 +1,12 @@
+﻿using HotelBooking.Db.Models;
+
+namespace HotelBooking.Db.Interfaces;
+
+/// <summary>
+/// Repository interface for managing city data in the database.
+/// </summary>
+public interface ICityRepository : IRepository<City>
+{
+    // Empty for now
+    // All CRUD comes from IRepository<City>
+}
---
+++ HotelBooking.Db/Repositories/CityRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotelBooking.Db.Data;
+using HotelBooking.Db.Interfaces;
+using HotelBooking.Db.Models;
+
+namespace HotelBooking.Db.Repositories;
+
+/// <summary>
+/// Repository implementation for managing city data in the database.
+/// </summary>
+public class CityRepository : Repository<City>, IRepository<City>
+{
+    public CityRepository(HotelBookingDbContext context) : base(context) { }
+}
