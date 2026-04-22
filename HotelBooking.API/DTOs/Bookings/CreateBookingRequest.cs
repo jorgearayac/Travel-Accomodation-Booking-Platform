eac1475973b72654dd6f54fb618f5fb940aa6aa9
jorgearayac <araya.cepeda.jorge@gmail.com>
2026-04-22 17:21:42 -0400

    add: Bookings DTOs, and Repository

---
+++ HotelBooking.API/DTOs/Bookings/CreateBookingRequest.cs
@@ -0,0 +1,19 @@
+﻿using HotelBooking.Db.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBooking.API.DTOs.Bookings;
+
+public class CreateBookingRequest
+{
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    
+    [Range(1, 10)]
+    public int NumberOfAdults { get; set; }
+
+    [Range(0, 10)]
+    public int NumberOfChildren { get; set; }
+    public required string PaymentMethod { get; set; }
+    public string? SpecialRequests { get; set; }
+    public required List<int> RoomIds { get; set; }
+}
---
+++ HotelBooking.Db/Interfaces/IBookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using HotelBooking.Db.Models;
+
+namespace HotelBooking.Db.Interfaces;
+
+public interface IBookingRepository : IRepository<Booking>
+{
+    Task<Booking?> GetByIdWithDetailsAsync(int id);
+    Task<IEnumerable<Booking>> GetByUserIdAsync(int userId);
+}
